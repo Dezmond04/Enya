b23b5947378adc8ff7654b4972c243ffc52dcde1
Dezmond04 <Dezmond04@mail.ru>
2021-06-04 18:17:02 +0300

    create: unreleased block

--- js/main.js
+++ js/main.js
@@ -5,10 +5,26 @@ const categorySlider = new Swiper(".category-slider", {
 	},
 	// Optional parameters
 	slidesPerView: 4,
-	spaceBetween: 30,
+	spaceBetween: 28,
 	loop: true,
 	// Navigation arrows
 	navigation: {
 		nextEl: ".category-slider__button--next",
+	},
+});
+
+const unreleasedSlider = new Swiper(".unreleased-slider", {
+	keyboard: {
+		enabled: true,
+		onlyInViewport: false,
+	},
+	// Optional parameters
+	slidesPerView: 5,
+	spaceBetween: 32,
+	loop: false,
+	// Navigation arrows
+	navigation: {
+		nextEl: ".unreleased-slider__button--next",
+		prevEl: ".unreleased-slider__button--prev",
 	},
 });
